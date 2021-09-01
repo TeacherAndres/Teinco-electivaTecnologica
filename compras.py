c125ef8c050e92b81dbcd28161e79acb6dbdd033
fabianlimas <63934412+fabianlimas@users.noreply.github.com>
2021-09-01 10:35:19 -0500

    Actualizacion
    Atualizacion compras

--- compras.py
+++ compras.py
@@ -9,7 +9,7 @@
 InfoCompra= input("Digite que va comprar: ")
 CantCompra= int(input("Digite la cantidad a comprar: "))
 ValCompra= int(input("Digite el valor de la compra: "))
-MedCompra= int(input("Por que medio compra?: "))
+MedCompra= int(input("Por que medio compra?: 1= Presencial\n 2=Internet\n3=otro"))
 Puntos=0
 for x in range(CantCompra):
     if(MedCompra=="1"):
@@ -19,9 +19,13 @@ for x in range(CantCompra):
         print("Usted sumo: ",Puntos ,"  puntos")
     elif (MedCompra=="2"):
         ValCompra+=ValCompra
-        print("Usted compro: ", InfoCompra," Veces por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
-        print("Usted sumo: ",(Puntos))
+        Puntos+=6
+        print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
+        print("Usted sumo: ",Puntos ,"  puntos")
+        
+
     elif (MedCompra=="3"):
         ValCompra+=ValCompra
-        print("Usted compro: ", InfoCompra," ",CantCompra," Veces por un valor de: ",(ValCompra*CantCompra), "por el medio de: ",MedCompra)
-        print("Usted sumo: ",(CantCompra*6))
+        Puntos+=1
+        print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
+        print("Usted sumo: ",Puntos ,"  puntos")
