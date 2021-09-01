9340e045d9e64352fceedd65bea60f26bf360023
Isaaacjuan877 <89918879+Isaaacjuan877@users.noreply.github.com>
2021-09-01 11:13:25 -0500

    Archivo terminado
    Archivo Completo

--- compras.py
+++ compras.py
@@ -6,26 +6,28 @@
 #internet 6, 
 
 
-InfoCompra= input("Digite que va comprar: ")
+
 CantCompra= int(input("Digite la cantidad a comprar: "))
-ValCompra= int(input("Digite el valor de la compra: "))
-MedCompra= int(input("Por que medio compra?:\n 1= Presencial\n 2=Internet\n 3=otro\n"))
+
 Puntos=0
 for x in range(CantCompra):
+    InfoCompra= input("Digite que va comprar: ")
+    ValCompra= int(input("Digite el valor de la compra: "))
+    MedCompra= int(input("Por que medio compra?:\n 1= Presencial\n 2=Internet\n 3=otro\n"))
     if(MedCompra==1):
         ValCompra+=ValCompra
         Puntos+=10
-        print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
-        print("Usted sumo: ",Puntos ,"  puntos")
+        print("Usted compro: ", InfoCompra,", por el medio de la opcion  : \n 1  Presencial\n 2 Internet\n 3 otro\n" ,MedCompra," Precio total acumulado: ",(ValCompra))
+        print("Usted sumo: ",Puntos ,"  puntos acumulables")
     elif (MedCompra==2):
         ValCompra+=ValCompra
         Puntos+=6
-        print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
-        print("Usted sumo: ",Puntos ,"  puntos")
+        print("Usted compro: ", InfoCompra,", por el medio de la opcion  : \n 1  Presencial\n 2 Internet\n 3 otro\n" ,MedCompra," Precio total acumulado: ",(ValCompra))
+        print("Usted sumo: ",Puntos ,"  puntos acumulables")
         
 
     elif (MedCompra==3):
         ValCompra+=ValCompra
         Puntos+=1
-        print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
-        print("Usted sumo: ",Puntos ,"  puntos")
+        print("Usted compro: ", InfoCompra,", por el medio de la opcion  : \n 1  Presencial\n 2 Internet\n 3 otro\n" ,MedCompra," Precio total acumulado: ",(ValCompra))
+        print("Usted sumo: ",Puntos ,"  puntos acumulables")
