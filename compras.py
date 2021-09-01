e25eb5c4b9d941088023385e0f6681b297a87e3e
Isaaacjuan877 <89918879+Isaaacjuan877@users.noreply.github.com>
2021-09-01 10:37:49 -0500

    correccion
    Correccion de la impresion

--- compras.py
+++ compras.py
@@ -9,22 +9,22 @@
 InfoCompra= input("Digite que va comprar: ")
 CantCompra= int(input("Digite la cantidad a comprar: "))
 ValCompra= int(input("Digite el valor de la compra: "))
-MedCompra= int(input("Por que medio compra?: 1= Presencial\n 2=Internet\n3=otro"))
+MedCompra= int(input("Por que medio compra?:\n 1= Presencial\n 2=Internet\n 3=otro\n"))
 Puntos=0
 for x in range(CantCompra):
-    if(MedCompra=="1"):
+    if(MedCompra==1):
         ValCompra+=ValCompra
         Puntos+=10
         print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
         print("Usted sumo: ",Puntos ,"  puntos")
-    elif (MedCompra=="2"):
+    elif (MedCompra==2):
         ValCompra+=ValCompra
         Puntos+=6
         print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
         print("Usted sumo: ",Puntos ,"  puntos")
         
 
-    elif (MedCompra=="3"):
+    elif (MedCompra==3):
         ValCompra+=ValCompra
         Puntos+=1
         print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
