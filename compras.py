e17ca12f74e17c7f9ac4eda51b6875791a0ac305
Isaaacjuan877 <89918879+Isaaacjuan877@users.noreply.github.com>
2021-09-01 10:19:24 -0500

    actualizacion
    Actualizacion  elif y otros

--- compras.py
+++ compras.py
@@ -9,19 +9,19 @@
 InfoCompra= input("Digite que va comprar: ")
 CantCompra= int(input("Digite la cantidad a comprar: "))
 ValCompra= int(input("Digite el valor de la compra: "))
-MedCompra= input("Por que medio compra?: ")
+MedCompra= int(input("Por que medio compra?: "))
 Puntos=0
 for x in range(CantCompra):
     if(MedCompra=="1"):
         ValCompra+=ValCompra
         Puntos+=10
-        print("Usted compro: ", InfoCompra," ValorCompra Veces por un valor de: ",(ValCompra), "por el medio de: ",MedCompra, "puntos: ",Puntos)
+        print("Usted compro: ", InfoCompra,"Valor"," por un valor de: ",(ValCompra), "por el medio de: ",MedCompra, "puntos: ",Puntos)
         print("Usted sumo: ",(CantCompra*6))
     elif (MedCompra=="2"):
         ValCompra+=ValCompra
         print("Usted compro: ", InfoCompra," Veces por un valor de: ",(ValCompra), "por el medio de: ",MedCompra)
         print("Usted sumo: ",(Puntos))
-    else:
+    elif (MedCompra=="3"):
         ValCompra+=ValCompra
         print("Usted compro: ", InfoCompra," ",CantCompra," Veces por un valor de: ",(ValCompra*CantCompra), "por el medio de: ",MedCompra)
         print("Usted sumo: ",(CantCompra*6))
