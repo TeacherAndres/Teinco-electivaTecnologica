b97211666275ed7558d79c570cdfb66941f4df9b
Isaaacjuan877 <89918879+Isaaacjuan877@users.noreply.github.com>
2021-09-01 11:33:55 -0500

    Archivo final optimizado
    Se optimiza el codigo y se elimina lineas innecesarias

--- compras.py
+++ compras.py
@@ -7,29 +7,21 @@
 
 
 
-CantCompra= int(input("Digite la cantidad a comprar: "))
+CantCompra= int(input("Digite la cantidad que compró: "))
 ValorTotalAcumulado=0
 Puntos=0
+
 for x in range(CantCompra):
    
     InfoCompra= input("Digite que compró: ")
     ValCompra= int(input("Digite el valor de la compra: "))
     MedCompra= int(input("Por que medio compra?:\n 1= Presencial\n 2=Internet\n 3=otro\n"))
     if(MedCompra==1):
-        ValorTotalAcumulado+=ValCompra  
         Puntos+=10
-        print("Usted compro: ", InfoCompra,", por el medio de la opcion  : \n 1  Presencial\n 2 Internet\n 3 otro\n" ,MedCompra," Precio total acumulado: ",ValorTotalAcumulado)
-        print("Usted sumo: ",Puntos ,"  puntos acumulables")
     elif (MedCompra==2):
-        ValorTotalAcumulado+=ValCompra 
         Puntos+=6
-        print("Usted compro: ", InfoCompra,", por el medio de la opcion  : \n 1  Presencial\n 2 Internet\n 3 otro\n" ,MedCompra," Precio total acumulado: ",ValorTotalAcumulado)
-        print("Usted sumo: ",Puntos ,"  puntos acumulables")
-        
-
     elif (MedCompra==3):
-        ValorTotalAcumulado+=ValCompra 
-        Puntos+=1
-        print("Usted compro: ", InfoCompra,", por el medio de la opcion  : \n 1  Presencial\n 2 Internet\n 3 otro\n" ,MedCompra," Precio total acumulado: ",ValorTotalAcumulado)
-        print("Usted sumo: ",Puntos ,"  puntos acumulables")
-    
+        Puntos+=1  
+    ValorTotalAcumulado+=ValCompra  
+    print("Usted compro: ", InfoCompra,", por el medio de la opcion  : \n 1  Presencial\n 2 Internet\n 3 otro\n" ,MedCompra," Precio total acumulado: ",ValorTotalAcumulado)
+    print("Usted sumo: ",Puntos ,"  puntos acumulables")
