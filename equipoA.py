06a5630fd51f05137fdc925cb3b4c6faadad7181
Andresleiva48 <1053867562@teinco.edu.co>
2021-09-01 09:54:25 -0500

    Cambio entradas

--- equipoA.py
+++ equipoA.py
@@ -2,6 +2,5 @@
 
 InfoCompra= input("Digite que va comprar: ")
 CantCompra= int(input("Digite la cantidad a comprar: "))
+ValCompra= int(input("Digite el valor de la compra: "))
 MedCompra= input("Por que medio compra?: ")
-
-
