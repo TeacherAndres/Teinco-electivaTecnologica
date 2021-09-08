b9b999cc8570e01d7731f346d1c87c5bfc8e0d9f
andresC-sup <camilosama6@gmail.com>
2021-09-08 09:33:28 -0500

    opcion profe
    opcion profe

---
+++ trabajoClaseFinal-Andres.py
@@ -0,0 +1,24 @@
+"""
+ El sistema debe componer por un menu con las sigientes opciones
+ 1) Registrar cuarto
+ 2) Consultar cuatros
+ 3) Salida del hotel
+ 4) Salir del hotel
+ 
+1) Debe registrar en el cuarto (pocision sigueinte del diccionario) 
+    Cuantas persdonas estaran en ese cuarto y por la n cantidad de personas registrar
+    Nombre - apellidos - telefono - email 
+    Mostrar los datos registrados apra confirmar
+
+2) Seben mostrar los caurtos ocupados y preguntar al usuario que cuarto quiere ver
+    Mostrar la cantida de pesonas en el cuarto y la infromacion de las mismas
+
+3) Se debe preguntar cual mcuarto se desocupa mostrar el valor a pagar y limpiar el cuaerto y sus ocupantes
+    calculo (Valor = 155.400 * persona )
+
+4)Salir del sistema
+"""
+
+dis = {'a':1,'b':2}
+print(dis)
+print(len(dis))
