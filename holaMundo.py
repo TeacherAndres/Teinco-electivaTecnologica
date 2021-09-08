55b1ff25ac5e5f8a151a7caecd2c21d53304a513
andresC-sup <camilosama6@gmail.com>
2021-09-08 07:59:58 -0500

    diccionarios
    lista de diccionario y funcionalidades

--- holaMundo.py
+++ holaMundo.py
@@ -1,6 +1,139 @@
-print("Hola")
-numU= 3
-NumD= 5
-Suma =numU+NumD
+"""
+print(prediccion({"nombre":"Manchester", "PG" : 5, "PE": 3, "PP": 2, "Ganador": 2, "GF": 38, "GC": 20, "Nomina": 800, "Continente": "Europa", "Posesion": 0.52}))
+//El equipo Manchester tiene el 0.9 de probabilidad de ganar el torneo
+print(prediccion({"nombre":"Boca", "PG" : 5, "PE": 2, "PP": 3, "Ganador": "3+", "GF": 20, "GC": 29, "Nomina": 600, "Continente": "Sudamerica", "Posesion": 0.45}))
+//El equipo Boca tiene el 0.4 de probabilidad de ganar el torneo
+print(prediccion({"nombre":"Zamalek", "PG" : 5, "PE": 2, "PP": 3, "Ganador": 0, "GF": 20, "GC": 29, "Nomina": 600, "Continente": "Africa", "Posesion": 0.45}))
+//El equipo Zamalek tiene el 0.4 de probabilidad de ganar el torneo
+print(prediccion({"nombre":"Manchester City", "PG" : 5, "PE": 3, "PP": 2, "Ganador": 0, "GF": 38, "GC": 20, "Nomina": 800, "Continente": "Europa", "Posesion": 0.52}))
+//El equipo Manchester City tiene el 0.8 de probabilidad de ganar el torneo
+"""
 
-print("Hello there")
+def prediccion(objeto):
+
+    if objeto["Continente"]=="Europa":
+
+        if (objeto["PG"]>objeto["PP"]) or (objeto["PE"]>objeto["PP"]):
+
+            if (objeto["PG"]>0) and (objeto["Posesion"]>0.5):
+                
+                if objeto["Ganador"]>0:
+                    return "El equipo  "+ objeto["nombre"]+ "  tiene 0.9 de probabilidad de ganar el torneo"
+                
+                else:
+
+                    if objeto["Nomina"]>700:
+                        return "El equipo  "+ objeto["nombre"]+ "  tiene 0.8 de probabilidad de ganar el torneo"
+
+                    else:
+                        return "El equipo  "+ objeto["nombre"]+ "  tiene 0.7 de probabilidad de ganar el torneo"
+
+            else:
+                return "El equipo  "+ objeto["nombre"]+ "  tiene 0.5 de probabilidad de ganar el torneo"
+
+        else:
+
+            if (objeto["PG"]>0) and (objeto["Posesion"]>0.4):
+                return "El equipo  "+ objeto["nombre"]+ "  tiene 0.5 de probabilidad de ganar el torneo"
+            
+            else:
+                return "El equipo  "+ objeto["nombre"]+ "  tiene 0.4 de probabilidad de ganar el torneo"
+
+    else: 
+
+        if objeto["Ganador"]>0:
+
+            if (objeto["PG"] > objeto["PP"]) and (objeto["PE"] > objeto["PP"]):
+
+                if (objeto["PG"]>0) or (objeto["Posesion"]>0.5):
+                    return "El equipo  "+ objeto["nombre"]+ "  tiene 0.85 de probabilidad de ganar el torneo"
+                
+                else:
+                    return "El equipo  "+ objeto["nombre"]+ "  tiene 0.8 de probabilidad de ganar el torneo"
+            
+            else:
+
+                if (objeto["Nomina"]>700) and (objeto["Posesion"]>0.4):
+                    return "El equipo  "+ objeto["nombre"]+ "  tiene 0.6 de probabilidad de ganar el torneo"
+                
+                else:
+                    return "El equipo  "+ objeto["nombre"]+ "  tiene 0.4 de probabilidad de ganar el torneo"
+            
+        else:
+
+            if objeto["Continente"]=="Sudamerica":
+
+                if objeto["PG"]>0:
+                    return "El equipo  "+ objeto["nombre"]+ "  tiene 0.6 de probabilidad de ganar el torneo"
+
+                else:
+                    return "El equipo  "+ objeto["nombre"]+ "  tiene 0.5 de probabilidad de ganar el torneo"
+
+            else:
+                    return "El equipo  "+ objeto["nombre"]+ "  tiene 0.4 de probabilidad de ganar el torneo"
+#PRIMER EJERCICIO
+
+
+print(prediccion({
+"nombre":"Manchester",
+"PG" : 5,
+"PE": 3,
+"PP": 2, 
+"Ganador": 2, 
+"GF": 38, 
+"GC": 20, 
+"Nomina": 800, 
+"Continente": "Europa", 
+"Posesion": 0.52
+}))
+
+
+#SEGUNDO EJERCICIO
+
+
+print(prediccion({
+"nombre":"Boca", 
+"PG" : 5, 
+"PE": 2,
+"PP": 3, 
+"Ganador": 3, 
+"GF": 20, 
+"GC": 29, 
+"Nomina": 600, 
+"Continente": "Sudamerica", 
+"Posesion": 0.45
+}))
+
+
+#TERCER EJERCICIO
+
+
+print(prediccion({
+"nombre":"Zamalek", 
+"PG" : 5, 
+"PE": 2, 
+"PP": 3, 
+"Ganador": 0, 
+"GF": 20, 
+"GC": 29, 
+"Nomina": 600, 
+"Continente": "Africa", 
+"Posesion": 0.45
+}))
+
+
+#CUARTO EJERCICIO
+
+
+print(prediccion({
+"nombre":"Manchester City", 
+"PG" : 5, 
+"PE": 3, 
+"PP": 2, 
+"Ganador": 0, 
+"GF": 38, 
+"GC": 20, 
+"Nomina": 800, 
+"Continente": "Europa", 
+"Posesion": 0.52
+}))
---
+++ profe-08-09-2021.py
@@ -0,0 +1,102 @@
+#Definir y crear diccionario (perro)
+perro = {'dueño': 'Federiko','raza':'canino','vacunas':['tosferina','rabia','cancha']}
+#Imprimir diccionario perro
+print(perro)
+#Imprimir valor por la llave
+print(perro['dueño'])
+#Imprimir valor por llave y pocision interna
+print(perro['vacunas'])
+print(perro['vacunas'][0])
+print(perro['vacunas'][2])
+#ZIP
+print("******************** ZIP ***************")
+diccionariobasico = dict(zip('abcd',[1,2,3,4]))
+print(diccionariobasico)
+#KEYS
+print("******************** KEYS ***************")
+keys=perro.keys()
+print(keys)
+#VALUES
+print("******************** VALUES ***************")
+valores=perro.values()
+print(valores)
+#COPY
+print("******************** COPY ***************")
+perro2 = perro.copy()
+print(perro2)
+#FROMKEYS
+print("******************** FROMKEYS ***************")
+perro3 = perro2.fromkeys(['a','b','c','d','e'],1)
+print(perro3)
+#get()
+print("******************** GET ***************")
+valorI = perro2.get('dueño')
+print(valorI)
+#pop()
+print("******************** POP ***************")
+dic = {'a':1,'b':2,'c':3,'d':4}
+print(dic)
+dic.pop('b')
+dic.pop('c')
+print(dic)
+#setdeault
+print("******************** setdeault ***************")
+dic = {'a':1,'b':2,'c':3,'d':4}
+valor = dic.setdefault('a')
+print(dic)
+valor = dic.setdefault('e',['a','b','c'])
+print(dic)
+print(dic['e'][2])
+#INSERT
+print("******************** INSERT ***************")
+datosUsr = []
+print(datosUsr)
+datosUsr.insert(0,'pepe')
+datosUsr.insert(1,'majarrez')
+datosUsr.insert(2,'limonche')
+print(datosUsr)
+#UPDATE
+print("******************** UPDATE ***************")
+dicI = {'a':1,'b':2,'c':3,'d':4}
+discS = {'a':1,'b':2,'e':3,'f':4}
+
+dicI.update(discS)
+print(dicI)
+dict= {'valorUltimo':'este es'}
+dicI.update(dict)
+print(dicI)
+# SEGUNDA FORMA DE CREAR UN DICCIONARIOS
+"""
+
+print("******************** CREAR DICCIONARIO SEGUNDA FORMA  ***************")
+d2 = dict([
+      ('Nombre', 'Sara'),
+      ('Edad', 27),
+      ('Documento', 1003882),
+])
+print (d2)
+"""
+#FOR
+print("******************** RECORRE DICCIONARIO ***************")
+for x in perro:
+    print(perro[x])
+
+#ANIDAR DICCIONARIO
+print("******************** ADICIONAR DICCIONARIO ***************")
+
+dicI = {'a':1,'b':2,'c':3,'d':4}
+discS = {'nombre':'Raul','apellido':'MElano','Telefono':312313,'direccion':'CALLE falas 123'}
+
+a = {
+    "dicLEtras":dicI,
+    "dicDatosPEr":discS
+}
+print(a)
+print("******************** ADICIONAR DICCIONARIO DENTRO DE DICCIONARIO ***************")
+
+b = {
+    "dicDatosPerro":perro,
+    "dicCumpuestoI":a
+}
+print(b)
+print(b.get('dicCumpuestoI'))
